6b42047362955533b34d5df8c9c315f657382efc
rajeshpokala <128123048+rajeshpokala@users.noreply.github.com>
2025-09-18 10:49:48 +0530

    Create addWPscript.js

---
+++ addWPscript.js
@@ -0,0 +1,5 @@
+const script = document.createElement('script');
+script.src = "https://d.oracleinfinity.io/infy/acs/account/8f18b0c3da08493b195f72e2f165b96a/js/perso_tag/odc.js";
+script.type = "text/javascript";
+script.async = false;
+document.head.appendChild(script);
